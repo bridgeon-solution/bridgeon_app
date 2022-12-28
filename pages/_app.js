636618f80645045901976c3ab3f9b0bbacbb1b56
majid-nope <muhammed0000majid@gmail.com>
2022-12-28 07:33:07 +0530

    update:dashboard

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
-import { wrapper } from '../store/storeConfig'
-import '../styles/globals.scss'
 
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Provider } from "react-redux";
+import { wrapper } from "../store/storeConfig";
+import "../styles/globals.scss";
+
+export default function App({ Component, ...rest }) {
+  const { store, props } = wrapper.useWrappedStore(rest);
+
+  return (
+    <Provider store={store}>
+    
+        <Component {...props.pageProps} />
+
+    </Provider>
+  );
 }
 
-export default wrapper.withRedux(App)
+
--- pages/components/layouts/sidebar/Sidebar.jsx
+++ pages/components/layouts/sidebar/Sidebar.jsx
@@ -1,19 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Sidebar.module.scss";
 import { Button } from "@mui/material";
 import Image from "next/image";
+import OverViewIcon from "@mui/icons-material/RemoveRedEyeOutlined";
+import AssignmentIcon from "@mui/icons-material/AssignmentTurnedInOutlined";
+import TaskIcon from "@mui/icons-material/TaskAltOutlined";
+import NotificationsIcon from "@mui/icons-material/NotificationsActiveOutlined";
+import SettingsIcon from "@mui/icons-material/SettingsOutlined";
 const Sidebar = () => {
-  const menu = ["Assignments", "Overview", "Tasks", "Notification", "Settings"];
+  const menu = [
+    {
+      title: "Overview",
+      icon: <OverViewIcon sx={{ justifySelf: "flex-start" }} />,
+    },
+    { title: "Assignments", icon: <AssignmentIcon /> },
+    { title: "Tasks", icon: <TaskIcon /> },
+    { title: "Notification", icon: <NotificationsIcon /> },
+    { title: "Settings", icon: <SettingsIcon /> },
+  ];
+
+  const [activeDashbord, setactiveDashbord] = useState(0);
+
+  let onDashbord = (event, indexOfDashboard) => {
+    setactiveDashbord(indexOfDashboard);
+    console.log(indexOfDashboard);
+  };
   return (
     <aside className={style.sidebar}>
       {Array(5)
         .fill()
         .map((i, index) => (
-          <span className={style.buttons} key={index}>{menu[index]}</span>
+          <span
+            className={[
+              style.buttons,
+              activeDashbord == index && style.active,
+            ].join(" ")}
+            key={index}
+            onClick={(e) => onDashbord(e, index)}
+          >
+            <div className={style.holder}>
+              {menu[index].icon}
+              {menu[index].title}
+            </div>
+          </span>
         ))}
       <span className={style.subscribe}>
-        <Image src="/assets/images/subscribe.svg" width={200} height={200}/>
-        <Button variant="contained" color="success">Subscribe</Button>
+        <Image
+          src="/assets/images/subscribe.svg"
+          width={200}
+          height={200}
+          alt="ad"
+        />
+        <Button variant="contained" color="success">
+          Subscribe
+        </Button>
       </span>
     </aside>
   );
