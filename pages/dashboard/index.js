ff754c3e7a91beaf302d8816d48d275ef5578085
majid-nope <muhammed0000majid@gmail.com>
2023-03-07 17:10:01 +0530

    fix:components-rule

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,12 +1,14 @@
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 
-const index = () => {
+const DashboardIndex = () => {
   const router = useRouter();
 
   useEffect(() => {
     router.push("/dashboard/overview");
   }, []);
+
+  return(<></>)
 };
 
-export default index;
+export default DashboardIndex;
